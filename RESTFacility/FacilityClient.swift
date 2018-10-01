bd67ccd8b368093bb69f90200112fada031c173c
santosh.kc <santosh.kc@samsung.com>
2018-10-01 18:44:44 +0530

    api modification

--- RESTFacility/FacilityClient.swift
+++ RESTFacility/FacilityClient.swift
@@ -36,8 +36,48 @@ class FacilityClient {
             return
         }
         
+        var requestURL: URL = url
+
+            if let newURL = URL(string: url.absoluteString + "?" + paramString) {
+                requestURL = newURL
+        }
         
+        var request: URLRequest? = URLRequest(url: requestURL)
+        request?.timeoutInterval = 10
+        request?.allHTTPHeaderFields
+        request?.httpMethod = requestMethod.rawValue
         
+ 
+    
+    }
+    
+    
+    getreques(needToSaveInterval: String? = nil, request: URLRequest, completion: ((JsonDictionary?, Error?) -> Void)?) {
+    requestQueue.addOperation { [weak self] in
+    let task = self?.session.dataTask(with: request) { [weak self] data, response, error in
+    
+        guard
+        error == nil,
+        let httpResponse = response as? HTTPURLResponse,
+        httpResponse.statusCode == 200,
+        let data = data,
+        let jsonData = try? JSONSerialization.jsonObject(with: data, options: []),
+        let json = jsonData as? JsonDictionary,
+        let rsp = json["rsp"] as? JsonDictionary
+        else {
+        if (response as? HTTPURLResponse)?.statusCode == 401 {
+        self?.token = nil
+        self?.requestQueue.isSuspended = true
+
+            }
+        completion?(nil, error)
+        return
+        }
+ 
+        completion?(rsp, nil)
+        }
+        task?.resume()
+    }
     }
 }
 
